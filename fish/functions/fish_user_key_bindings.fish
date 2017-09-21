27f5ae160f02e3d1d1fb4508b6ea0a96913a950b
tatsuroro <me@tatsuroro.com>
2017-09-21 17:50:45 +0900

    Mod fish shell config

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,9 @@ function fish_user_key_bindings
   bind \cr peco_select_history
   bind \cz peco_recentd
   bind \cq peco_kill
+  bind \cx\ct peco_todoist_item
+  bind \cx\cp peco_todoist_project
+  bind \cx\cl peco_todoist_labels
+  bind \cx\cc peco_todoist_close
+  bind \cx\cx peco_todoist_delete
 end
---
+++ fish/functions/peco_todoist_item.fish
@@ -0,0 +1,1 @@
+/Users/tatsuroro/.config/fisherman/peco_todoist/functions/peco_todoist_item.fish
