a0e49a332b212824a66fd3bc165dc8d521046b72
tatsuroro <me@tatsuroro.com>
2017-11-05 23:49:34 +0900

    Change default editor from nvim to vim

--- fish/env.fish
+++ fish/env.fish
@@ -1,5 +1,5 @@
 set -gx LANG ja_JP.UTF-8
-set -gx EDITOR nvim
+set -gx EDITOR vim
 set -gx SHELL /usr/local/bin/fish
 
 set -gx PATH /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin $PATH
@@ -9,8 +9,13 @@ set -gx PATH ./bin $HOME/bin $HOME/sh $PATH
 set -gx GOPATH $HOME
 set -gx PATH $PATH $GOPATH/bin /usr/local/opt/go/libexec/bin
 
+#rbenv
 set -gx PATH /usr/local/opt/rbenv/bin $PATH
 
+#flutter
+set -gx PATH ~/src/github.com/flutter/flutter/bin $PATH
+
+#homebrew cask
 set -gx HOMEBREW_CASK_OPTS "--appdir=/Applications"
 
 # for neovim
