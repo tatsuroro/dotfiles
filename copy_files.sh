aa1d6d39ee7d8db5f54795fbb1bb8654ac39ae52
Tatsuro Nakamura <me@tatsuroro.com>
2019-04-24 17:15:01 +0900

    Add git hooks: pre-commit username modifier

--- copy_files.sh
+++ copy_files.sh
@@ -4,16 +4,22 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cloud="Google\ Drive"
 devDir="$HOME/${cloud}/dev"
 
-for dirname in .config/karabiner/assets/complex_modifications .git_template/hooks .local/share src bin pkg
+for dirname in .config/karabiner/assets/complex_modifications .git_template/hooks .git/hooks .local/share src bin pkg
 do
   echo "mkdir -p \"$HOME/${dirname}\""
   mkdir -p "$HOME/${dirname}"
   chmod 755 "$HOME/${dirname}"
 done
 
-echo "+++ copy gitconfig"
+echo "+++ copy global gitconfig"
 echo "cp -f \"${dir}/dot/gitconfig\" \"$HOME/.gitconfig\""
 cp -f "${dir}/dot/gitconfig" "$HOME/.gitconfig"
+
+echo "+++ copy global git hooks"
+echo "cp -f \"${dir}/dot/git/hooks/*\" \"$HOME/.git/hooks\""
+cp -f "${dir}/dot/git/hooks/*" "$HOME/.git/hooks"
+chmod 755 "$HOME/.git/hooks/*"
+echo "[WARN] You should rename pre-commit hooks config (UserName & Email) !!!"
 
 echo "+++ copy ssh key & configs"
 echo "cp -r \"${devDir}/ssh\" \"$HOME/.ssh\""
