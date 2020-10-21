5e3da7e63f45f44d408f6321b7de57a3dc0837f6
Tatsuro Nakamura <me@tatsuroro.com>
2020-10-21 10:30:00 +0900

    Exec git pull after checkout with fzf function

--- dot/config/fish/functions/fzf_git_branch_checkout.fish
+++ dot/config/fish/functions/fzf_git_branch_checkout.fish
@@ -1,7 +1,7 @@
 function fzf_git_branch_checkout
   set -l selected (git branch | fzf)
 
-  [ -n "$selected" ] && eval "git checkout $selected"
+  [ -n "$selected" ] && eval "git checkout $selected" && eval "git pull"
 
   commandline -f repaint
 end
