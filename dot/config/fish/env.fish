04b68f6c90e9ff3a9ae476e9671b431a0364a7f7
Tatsuro Nakamura <me@tatsuroro.com>
2024-01-15 14:14:45 +0900

    Tweak path envs

--- dot/config/fish/env.fish
+++ dot/config/fish/env.fish
@@ -2,10 +2,6 @@ set -gx LANG ja_JP.UTF-8
 set -gx EDITOR nvim
 set -gx SHELL /opt/homebrew/bin/fish
 
-# homebrew
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
-
 # for neovim
 set -gx XDG_CONFIG_HOME $HOME/.config
 
@@ -13,15 +9,10 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx VOLTA_HOME $HOME/.volta
 
 # Java
-fish_add_path /opt/homebrew/opt/openjdk/bin
 set -gx CPPFLAGS "-I/opt/homebrew/opt/openjdk/include"
 
 # GoRoot
 set -gx GOPATH $HOME
-set -gx PATH $PATH $GOPATH/bin /opt/homebrew/opt/go/libexec/bin
-
-# flutter
-set -gx PATH $PATH $HOME/src/github.com/flutter/flutter/bin
 
 # deno
 set -gx PATH $PATH $HOME/.deno/bin
@@ -32,9 +23,6 @@ set -gx LDFLAGS "-L/opt/homebrew/opt/libpq/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/libpq/include"
 
 set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/libpq/lib/pkgconfig"
-
-# misc PATH
-set -gx PATH ./bin $HOME/sh $VOLTA_HOME/bin $PATH
 
 # ubie
 set -gx PATH $PATH $HOME/.ubie-bin
