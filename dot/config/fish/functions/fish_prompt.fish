a64b1cc47170bc5460bd52f0d5ba3392782b914f
Tatsuro Nakamura <me@tatsuroro.com>
2024-01-15 14:16:31 +0900

    Tweak fish prompt

--- dot/config/fish/functions/fish_prompt.fish
+++ dot/config/fish/functions/fish_prompt.fish
@@ -1,7 +1,3 @@
 function fish_prompt --description 'Write out the prompt'
-  echo -n (set_color cyan)(prompt_pwd)' '(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-end
-
-function fish_right_prompt
-  echo -n (fish_git_prompt)
+  echo -n (set_color cyan)(prompt_pwd)(fish_git_prompt)' '(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
