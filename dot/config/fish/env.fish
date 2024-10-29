d081d05bbd9520bd5e99c39fe22d06a74a492b70
Tatsuro Nakamura <me@tatsuroro.com>
2024-10-29 21:12:09 +0900

    Update skipcd: Add support for multiple directories

--- dot/config/fish/env.fish
+++ dot/config/fish/env.fish
@@ -2,7 +2,7 @@ set -gx LANG ja_JP.UTF-8
 set -gx EDITOR nvim
 set -gx SHELL /opt/homebrew/bin/fish
 
-# for neovim
+# neovim
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 # volta
@@ -26,9 +26,14 @@ set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/libpq/lib/pkgconfig"
 # pyenv
 set -gx PYENV_ROOT $HOME/.pyenv
 
+# rancherd
+set -gx PATH $PATH $HOME/.rd/bin
+
 # ubie
 set -gx PATH $PATH $HOME/.ubie-bin
 
 # skipcd
-set -gx SKIPCD_REPO_PATH $HOME/src/github.com/ubie-inc/genpaku
-set -gx SKIPCD_SUBDIR_NAME client
+set -gx SKIPCD_REPO_PATH $HOME/src/github.com/ubie-inc/genpaku,$HOME/src/github.com/AOI-Inc/AIDocumentWorks
+set -gx SKIPCD_SUBDIR_NAME client,webapp
+
+set -gx USE_GKE_GCLOUD_AUTH_PLUGIN True
