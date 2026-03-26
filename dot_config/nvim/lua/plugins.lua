63e7b663c27e6949b21e48a922bd72a91dd131ab
Tatsuro Nakamura <me@tatsuroro.com>
2026-03-26 18:08:30 +0900

    Remove macism / im-select.nvim and update Brewfile

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -37,19 +37,6 @@ require('pckr').add{
   -- surrounding
   "tpope/vim-surround";
 
-  -- input mode handler
-  {
-    "keaising/im-select.nvim",
-    config = function()
-      require("im_select").setup({
-        default_im_select = "com.apple.keylayout.ABC",
-        default_command = "macism",
-        set_default_events = { "InsertLeave", "CmdlineLeave" },
-        set_previous_events = { "InsertEnter" },
-        async_switch_im = true,
-      })
-    end
-  };
 
   -- Close buffer without break window layout
   "famiu/bufdelete.nvim";
